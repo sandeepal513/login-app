80f78a9bc123ca881657ab0ce450e84439387713
sandeepal513 <sandeepal513@gmail.com>
2026-01-18 10:07:07 +0530

    create mern project and install other packages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Homepage</h1>
+        <button >Get Start</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
